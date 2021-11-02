28bc62661db9e5f9c695532aedfe4807245bb299
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-02 13:29:39 +0200

    Add Navbar router links and update styling.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
 
 function Navbar() {
   return (
     <nav className="navbar">
       <header>
-        <h1>Bookstore CMS</h1>
+        <h1>
+          <Link to="/" className="header-link">Bookstore CMS</Link>
+        </h1>
       </header>
       <ul className="nav-list">
-        <li>BOOKS</li>
-        <li>CATEGORIES</li>
+        <li>
+          <Link to="/books" className="link">BOOKS</Link>
+        </li>
+        <li>
+          <Link to="/categories" className="link">CATEGORIES</Link>
+        </li>
       </ul>
     </nav>
   );
