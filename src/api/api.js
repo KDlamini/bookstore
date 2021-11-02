2ce26a31ff50fb8d67b4bcd22f40935d20a361d5
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-02 19:47:31 +0200

    Create 'api' component that will use 'getBooks' to get mock data.

---
+++ src/api/api.js
@@ -0,0 +1,12 @@
+function getBooks() {
+  return [
+    {
+      id: 'sdasas42323', title: 'The Hunger Games', author: 'Suzanne Collins', genre: 'Dystopian Fiction',
+    },
+    {
+      id: 'gjghjf34235', title: 'Harry Potter and The Half-Blood Prince', author: 'J.K Rowling', genre: 'Fantasy',
+    },
+  ];
+}
+
+export default getBooks;
