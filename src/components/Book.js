63c5e919bf990b1eedc6f0f0e4feaa270ef61130
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-02 20:25:20 +0200

    Map Book component using data from mock api.

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../styles/Book.css';
 
-function Book() {
+function Book({ book }) {
   return (
     <div className="book-container">
-      <h3>Genre</h3>
-      <h2>Book name</h2>
-      <p>Author name</p>
+      <h3>{book.genre}</h3>
+      <h2>{book.title}</h2>
+      <p>{book.author}</p>
       <ul className="book-actions">
         <li>
           <button type="button" className="action comments">Comments</button>
@@ -21,5 +22,19 @@ function Book() {
     </div>
   );
 }
+
+Book.propTypes = {
+  book: PropTypes.shape({
+    title: PropTypes.string,
+    author: PropTypes.string,
+    genre: PropTypes.string,
+  }),
+};
+
+Book.defaultProps = {
+  book: {
+    id: 'sdasas42323', title: 'The Hunger Games', author: 'Suzanne Collins', genre: 'Dystopian Fiction',
+  },
+};
 
 export default Book;
--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Book from '../components/Book';
 import AddBook from '../components/AddBook';
+import getBooks from '../api/api';
 
 function Books() {
+  const books = getBooks();
+
   return (
     <section className="books-page">
-      <Book />
+      {
+        books.map((book) => <Book key={book.id} book={book} />)
+      }
       <AddBook />
     </section>
   );
