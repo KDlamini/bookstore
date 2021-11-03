bfd9b08f8516cbe89d58ebb03321bf03702f907e
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-03 15:09:22 +0200

    OnClick add new book on the page.

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from '../components/Book';
 import AddBook from '../components/AddBook';
 import getBooks from '../api/api';
 
 function Books() {
   const books = getBooks();
+  const state = useSelector((state) => state.booksReducer);
+
+  state.map((book) => books.push(book));
 
   return (
     <section className="books-page">
