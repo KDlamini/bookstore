384aa344ae37d61e715eb0aa321d2cf322d50f2c
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-03 13:34:28 +0200

    Setup action types, reducer, and contants for Books. - Dispatch new book on AddBook component. - Set Provider to make Store available in entire app. - return new state on AddBook button click.

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
 function AddBook() {
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+  const dispatch = useDispatch();
+
+  const submitBookToStore = (title, author) => {
+    const id = title + Math.floor(Math.random() * (1000 - 100 + 1) + 100);
+
+    const newBook = {
+      id,
+      title,
+      author,
+    };
+
+    dispatch(addBook(newBook));
+  };
+
   return (
     <form>
       <h2>ADD NEW BOOK</h2>
-      <input type="text" placeholder="Book title" />
-      <input type="dropdown" placeholder="Category" />
-      <button type="submit">ADD BOOK</button>
+      <input
+        type="text"
+        placeholder="Book title"
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      <input
+        type="dropdown"
+        placeholder="Category"
+        onChange={(e) => setCategory(e.target.value)}
+      />
+
+      <button
+        type="submit"
+        onSubmit={() => submitBookToStore(title, category)}
+      >
+        ADD BOOK
+      </button>
     </form>
   );
 }
---
+++ src/redux/books/books.js
@@ -0,0 +1,35 @@
+/* eslint-disable consistent-return */
+const ADD_BOOK = 'bookStore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+
+const initialState = [];
+
+export const addBook = (payload) => ({
+  type: ADD_BOOK,
+  payload,
+});
+
+export const removeBook = (payload) => ({
+  type: REMOVE_BOOK,
+  payload,
+});
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.payload];
+
+    case REMOVE_BOOK:
+      /*
+        use ES6 filter() method to create a new array,
+        which will not contain the book you want to remove from the store
+        (filter by the id key - i.e.:
+        return state.filter(book => book.id !== id);
+      */
+      break;
+    default:
+      return state;
+  }
+};
+
+export default reducer;
