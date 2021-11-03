4408dd8136540cf81f02ca186265d8da5f8c371e
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-03 15:55:56 +0200

    Set initial state to 'getBooks' from api.

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -2,13 +2,9 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import Book from '../components/Book';
 import AddBook from '../components/AddBook';
-import getBooks from '../api/api';
 
 function Books() {
-  const books = getBooks();
-  const state = useSelector((state) => state.booksReducer);
-
-  state.map((book) => books.push(book));
+  const books = useSelector((state) => state.booksReducer);
 
   return (
     <section className="books-page">
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,8 +1,10 @@
 /* eslint-disable consistent-return */
+import getBooks from '../../api/api';
+
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
-const initialState = [];
+const initialState = getBooks();
 
 export const addBook = (payload) => ({
   type: ADD_BOOK,
